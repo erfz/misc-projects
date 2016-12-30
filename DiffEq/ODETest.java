06574457d486e1aeccea1912b5feb999f0ee61cb
Jason <exceptionalqualities@gmail.com>
2016-12-30 12:15:26 -0500

    Update ODETest.java

--- DiffEq/ODETest.java
+++ DiffEq/ODETest.java
@@ -1,17 +1,19 @@
 public class ODETest{
 	public static void main(String[] args){
-		double x = 1;
-		double dt = 0.000000001;
-		double totaldt = .9;
-		double t = 0;
-		double i = 0;
-		while (i < totaldt/dt){
-			x += x*t*dt;
-			t += dt;
+		double t = 0; // initial condition
+		double x = 1; // x(t_0) = x_0
+		
+		double dt = 0.000000001; // step size
+		double totaldt = .9; // total change in t -- final t = t_0 + totaldt
+		
+		double i = 0; // variable of iteration
+		while (i < totaldt/dt){ // totaldt/dt iterations in total
+			x += x*t*dt; // x = x + dx = x + dt(dx/dt) -- here dt is finite so in actuality this is approximate
+			t += dt; // iterate to next t value
 			++i;
 		}
 		System.out.println(x);
 	}
 }
 
-// holy fuck this works...
+// Euler's Method
